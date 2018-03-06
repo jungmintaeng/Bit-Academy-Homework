dbdc6d5eef8ec54886b5db9c562e9c906584f0d1
jungmintaeng <jungmin4025@gmail.com>
2018-03-06 16:37:48 +0900

    sql hw commit

---
+++ 기본.sql
@@ -0,0 +1,55 @@
+-- 문제 1
+select concat(first_name, ' ' , last_name) as '이름'
+from employees
+where emp_no=10944;
+
+-- 문제 2
+select concat(first_name, ' ' , last_name) as '이름',
+		gender as '성별',
+        hire_date as '입사일'
+from employees
+order by hire_date;
+
+-- 문제 3
+select gender, count(*)
+from employees
+group by gender;
+
+-- 문제 4
+select count(distinct emp_no)
+from salaries;
+
+-- 문제 5
+select count(distinct dept_no)
+from departments;
+
+-- 문제 6
+select count(*)
+from dept_manager
+where from_date <= now() and to_date >= now();
+
+-- 문제 7
+select dept_name
+from departments
+order by length(dept_name) desc;
+
+-- 문제 8
+select count(*)
+from salaries
+where from_date <= now() and to_date >= now() and salary >= 120000;
+
+-- 문제 9
+select distinct title
+from titles
+order by length(title) desc;
+
+-- 문제 10
+select count(*)
+from titles
+where from_date <= now() and to_date >= now() and lower(title) like '%engineer';
+
+-- 문제 11
+select title
+from titles
+where emp_no=13250
+order by from_date asc;
---
+++ 집계.sql
@@ -0,0 +1,31 @@
+-- 문제 1
+select max(salary) as '최고임금',
+		min(salary) as '최저임금',
+		max(salary)-min(salary) as '최고임금-최저임금'
+from salaries;
+
+-- 문제 2
+select date_format(max(hire_date), '%Y년 %m월 %d일')
+from employees;
+
+-- 문제 3
+select date_format(min(hire_date), '%Y년 %m월 %d일')
+from employees;
+
+-- 문제 4
+select avg(salary)
+from salaries;
+
+-- 문제 5
+select max(salary) as '최고연봉',
+		min(salary) as '최저연봉'
+from salaries
+where from_date <= now() and to_date >= now();
+
+-- 문제 6
+select
+	convert(period_diff(date_format(now(), '%Y%m'),date_format(max(birth_date), '%Y%m'))/12, signed)
+    as '최고 어린 사원',
+    convert(period_diff(date_format(now(), '%Y%m'),date_format(min(birth_date), '%Y%m'))/12, signed)
+    as '연장자'
+from employees;
