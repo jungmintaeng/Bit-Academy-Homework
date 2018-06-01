3672e11f2e3915452a6300d681dc2829ae8c6ad3
jungmintaeng <jungmin4025@gmail.com>
2018-06-01 14:04:07 +0900

    완

--- collection/api/api.py
+++ collection/api/api.py
@@ -28,19 +28,10 @@ def pd_fetch_tourspot_visitor(
                          RES_NM=tourspot,
                          pageNo=i)
         json_result = json_request(url, 'utf-8')
-        # json_result = json.loads('{"response":{"header":{"resultCode":"0000","resultMsg":"OK"},"body":{"items":{"item":[{"addrCd":2635,"csForCnt":286,"csNatCnt":9110,"gungu":"해운대구","resNm":"부산시립미술관","rnum":1,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":80,"csNatCnt":937,"gungu":"해운대구","resNm":"M.Do 관광호텔","rnum":2,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":7004,"csNatCnt":16473,"gungu":"해운대구","resNm":"파라다이스호텔부산","rnum":3,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":59,"csNatCnt":1436,"gungu":"해운대구","resNm":"호텔일루아","rnum":4,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":2063,"csNatCnt":4830,"gungu":"해운대구","resNm":"해운대그랜드호텔","rnum":5,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":1403,"csNatCnt":6012,"gungu":"해운대구","resNm":"부산웨스턴조선호텔","rnum":6,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":712,"csNatCnt":17025,"gungu":"해운대구","resNm":"해운대 글로리콘도","rnum":7,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":1113,"csNatCnt":5736,"gungu":"해운대구","resNm":"노보텔부산","rnum":8,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":9610,"csNatCnt":37223,"gungu":"해운대구","resNm":"해운대 한화리조트 부산","rnum":9,"sido":"부산광역시","ym":201201},{"addrCd":2635,"csForCnt":73,"csNatCnt":1188,"gungu":"해운대구","resNm":"송정관광호텔","rnum":10,"sido":"부산광역시","ym":201201}]},"numOfRows":10,"pageNo":1,"totalCount":10}}}')
         try:
             total_count = json_result.get('response').get('body').get('totalCount')
             data = json_result.get('response').get('body').get('items').get('item')
-            for item in data:
-                parsed_data = dict()
-                parsed_data['count_locals'] = item.get('csNatCnt', '')
-                parsed_data['count_foreigner'] = item.get('csForCnt', '')
-                parsed_data['tourist_spot'] = item.get('resNm', '')
-                parsed_data['date'] = item.get('ym', '')
-                parsed_data['restrict1'] = item.get('sido', '')
-                parsed_data['restrict2'] = item.get('gungu', '')
-                result.append(parsed_data)
+            result += data
         except AttributeError:
             return []
         if total_count <= 10 * i:
--- collection/crawler.py
+++ collection/crawler.py
@@ -4,8 +4,18 @@ import json
 from datetime import datetime
 
 
-def preprocess_tourspot_visitor(item):
-    pass
+def preprocess_tourspot_visitor(data):
+    result = []
+    for item in data:
+        parsed_data = dict()
+        parsed_data['count_locals'] = item.get('csNatCnt', '')
+        parsed_data['count_foreigner'] = item.get('csForCnt', '')
+        parsed_data['tourist_spot'] = item.get('resNm', '')
+        parsed_data['date'] = item.get('ym', '')
+        parsed_data['restrict1'] = item.get('sido', '')
+        parsed_data['restrict2'] = item.get('gungu', '')
+        result.append(parsed_data)
+    return result
 
 
 def preprocess_foreign_visitor(data):
@@ -24,6 +34,7 @@ def crawling_tourspot_visitor(
         restore_directory=None,
         service_key=None,
         fetch=True):
+    result = []
     file_name = '{0}/{1}_tourinstspot_{2}_{3}.json'.format(
         CONFIG['common']['restore_directory'] if restore_directory is None else restore_directory, district, start_year,
         end_year)
@@ -38,7 +49,8 @@ def crawling_tourspot_visitor(
         for month in range(1, 13):
             if year == now_year and month > now_month:
                 continue
-            result = pd_fetch_tourspot_visitor(district1=district, year=year, month=month, service_key=service_key)
+            data = pd_fetch_tourspot_visitor(district1=district, year=year, month=month, service_key=service_key)
+            result += preprocess_tourspot_visitor(data)
     with open(file_name, 'w', encoding='utf-8') as outfile:
         json_string = json.dumps(result, indent=4, sort_keys=True, ensure_ascii=False)
         outfile.write(json_string)
